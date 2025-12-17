8bc0a4c7ad65ac9278fc13532e2bf1fce674e2ee
Rochak jain <145226358+rochaksinghai@users.noreply.github.com>
2025-12-17 22:03:09 +0530

    Add files via upload

---
+++ Pattern_programming.java
@@ -0,0 +1,82 @@
+package domain;
+
+public class Pattern_programming {
+	
+	public static void main(String[] args) {
+		
+	/*
+	                       ****
+                           ****
+                           ****
+                           ****
+
+	 */
+		
+	/*	for (int i = 1; i <= 4; i++) {        // rows
+            for (int j = 1; j <= 4; j++) {    // columns
+                System.out.print("*");
+            }
+            System.out.println(); // next line
+        }
+		
+	*/
+		
+		
+		
+	/*
+	                  *
+                      **
+                      ***
+                      ****
+
+	 */
+		
+	/*
+		for (int i = 1; i <= 4; i++) {        // rows
+            for (int j = 1; j <= i; j++) {    // stars per row
+                System.out.print("*");
+            }
+            System.out.println(); // next line
+        }
+        
+    */
+		
+	/*
+	                                   1 
+                                       2 2 
+                                       3 3 3 
+                                       4 4 4 4 
+	
+	 */
+		
+	/*
+		for (int i = 1; i <= 4; i++) {        // rows
+            for (int j = 1; j <= i; j++) {    // stars per row
+                System.out.print(i + " ");
+            }
+            System.out.println(); // next line
+        }
+    */
+		
+		
+		
+	/*
+	                                   1 
+                                       1 2 
+                                       1 2 3 
+                                       1 2 3 4  
+	*/
+		
+	/*
+	 
+		for (int i = 1; i <= 4; i++) {        // rows
+            for (int j = 1; j <= i; j++) {    // stars per row
+                System.out.print(j + " ");
+            }
+            System.out.println(); // next line
+        }
+        
+    */
+		
+  }
+}
